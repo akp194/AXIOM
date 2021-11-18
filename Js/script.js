986660b0664a69b18dca3554452e1e4847398489
royalboss <ayushmunot921@gmail.com>
2021-11-18 23:38:47 +0530

    first commit

--- Js/script.js
+++ Js/script.js
@@ -3,6 +3,10 @@ function makeitfull(event) {
     if (ele.classList.contains('fullcard')) {
         ele.classList.remove('fullcard');
     } else {
+        var eles = document.querySelectorAll('li.fullcard');
+        if (eles.length) {
+            eles[0].classList.remove('fullcard');
+        }
         ele.classList.add('fullcard');
     }
 }
@@ -24,3 +28,57 @@ function swiperight() {
         eles[i].setAttribute('data-card', (parseInt(eles[i].getAttribute('data-card')) + 3) % 5 + 1);
     }
 }
+
+function showontop(event) {
+    cname = event.target.dataset.cardname;
+    var ele1 = document.querySelector(`.slides li.${cname}`);
+    var ele2 = document.querySelector(`.slides li[data-card="3"]`);
+    ele2.setAttribute("data-card", ele1.dataset.card);
+    ele1.setAttribute("data-card", "3");
+
+    if (ele2.classList.contains("fullcard")) {
+        ele2.classList.remove("fullcard");
+        ele1.classList.add("fullcard");
+    }
+}
+
+
+function leftswipe() {
+    swipeleft();
+}
+
+function rightswipe() {
+    swiperight();
+}
+
+var startingx, startingy, movingx, movingy;
+function touchStart(evt) {
+    startingx = evt.touches[0].clientX;
+    startingy = evt.touches[0].clientY;
+}
+
+function touchMove(evt) {
+    movingx = evt.touches[0].clientX;
+    movingy = evt.touches[0].clientY;
+}
+
+function touchEnd() {
+    // alert(startingx + 100 < movingx); //right
+    // alert(startingx - 100 > movingx); //left
+    // alert(startingy - 100 > movingy); //up
+    // alert(startingy + 100 < movingy); //down
+
+    if (Math.abs(startingx - movingx) > 100) {
+        if (startingx + 100 < movingx) {
+            rightswipe();
+        }
+        else if (startingx - 100 > movingx) {
+            leftswipe();
+        }
+    }
+    startingx = undefined;
+    startingy = undefined;
+    movingx = undefined;
+    movingy = undefined;
+}
+
